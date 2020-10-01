fec99def32f4ff3eb483c0c7e2d703e2a2c638fc
Felippe Augusto Alacoque Rodrigues Mariano <felippealacoque@gmail.com>
2020-09-30 23:55:21 -0300

    Atualização de dados do cliente

--- ApiDti/src/main/java/com/br/dti/service/ClienteService.java
+++ ApiDti/src/main/java/com/br/dti/service/ClienteService.java
@@ -2,10 +2,12 @@ package com.br.dti.service;
 
 import org.springframework.validation.BindingResult;
 
+import com.br.dti.dto.ClienteDTO;
 import com.br.dti.model.Cliente;
 
 public interface ClienteService {
 	
 	Cliente persistir (Cliente cliente);
 	void validarClienteExistente (String cpf, BindingResult result);
+	void atualizarCliente(Cliente cliente, ClienteDTO clienteDto, BindingResult result);
 }
--- ApiDti/src/main/java/com/br/dti/service/serviceImpl/ClienteServiceImpl.java
+++ ApiDti/src/main/java/com/br/dti/service/serviceImpl/ClienteServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 
+import com.br.dti.dto.ClienteDTO;
 import com.br.dti.model.Cliente;
 import com.br.dti.repository.ClienteRepository;
 import com.br.dti.service.ClienteService;
@@ -23,6 +24,21 @@ public class ClienteServiceImpl implements ClienteService {
 	@Override
 	public void validarClienteExistente(String cpf, BindingResult result) {
 		clienteRepository.findByCpf(cpf).ifPresent(cli -> result.addError(new ObjectError("cliente", "Já existe um cliente cadastrado com esse CPF")));
+	}
+
+	@Override
+	public void atualizarCliente(Cliente cliente, ClienteDTO clienteDto, BindingResult result) {
+		cliente.setCelular(clienteDto.getCelular());
+		cliente.setEmail(clienteDto.getEmail());
+		cliente.setEndereco(clienteDto.getEndereco());
+		cliente.setNome(clienteDto.getNome());
+		
+		if(!cliente.getCpf().equals(clienteDto.getCpf())) {
+			clienteRepository.findByCpf(clienteDto.getCpf())
+				.ifPresent(cli -> result.addError(new ObjectError("cliente", "Já existe cliente cadastrado com o CPF " + cliente.getCpf())));
+			cliente.setCpf(clienteDto.getCpf());
+		}
+		
 	}
 
 
